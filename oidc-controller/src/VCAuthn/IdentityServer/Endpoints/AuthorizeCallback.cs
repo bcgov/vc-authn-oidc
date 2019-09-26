335d860bfe87672e0c10c1f9c88736ecd950ba07
Tobias Looker <tplooker@gmail.com>
2019-09-27 11:11:29 +1200

    Remove incomplete functionality
    Signed-off-by: Tobias Looker <tplooker@gmail.com>

--- oidc-controller/src/VCAuthn/IdentityServer/Endpoints/AuthorizeCallback.cs
+++ oidc-controller/src/VCAuthn/IdentityServer/Endpoints/AuthorizeCallback.cs
@@ -61,23 +61,7 @@ namespace VCAuthn.IdentityServer.Endpoints
                 return new RedirectResult(url);
             }
 
-            //if (session.RequestParameters[IdentityConstants.ResponseTypeUriParameterName] == "token")
-            //{
-            //    _logger.LogDebug("Token flow. Creating a token");
-            //    var presentation = session.Presentation;
-            //    var issuer = context.GetIdentityServerIssuerUri();
-            //    var token = await _tokenIssuerService.IssueJwtAsync(10000, issuer, session.PresentationRecordId, presentation); //TODO parameterize token issuance
-
-            //    if (_sessionStorageService.DeleteSession(session) == false)
-            //    {
-            //        _logger.LogError("Failed to delete a session");
-            //    }
-
-            //    var url = $"{session.RequestParameters[IdentityConstants.RedirectUriParameterName]}#access_token ={token}&token_type=Bearer";
-            //    _logger.LogDebug($"Token flow. Redirecting to {url}");
-
-            //    return new RedirectResult(url);
-            //}
+            //TODO add token flow handling
 
             _logger.LogError("Unknown response type");
             return VCResponseHelpers.Error("invalid_response_type", $"Unknown response type: [{session.RequestParameters[IdentityConstants.ResponseModeUriParameterName]}]");
