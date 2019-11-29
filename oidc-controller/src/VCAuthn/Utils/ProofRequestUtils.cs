c7cd60721954fe3bc80c0bbc2ede4d33077d5a93
Emiliano Suñé <emiliano.sune@gmail.com>
2019-11-29 15:01:45 -0800

    Code refactoring
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- oidc-controller/src/VCAuthn/Utils/ProofRequestUtils.cs
+++ oidc-controller/src/VCAuthn/Utils/ProofRequestUtils.cs
@@ -37,15 +37,5 @@ namespace VCAuthn.Utils
         }
     }
 
-    class ProofRequest_v_1_0
-    {
-        [JsonProperty("version")]
-        public string Version { get; set; }
-
-        [JsonProperty("requested_attributes")]
-        public Dictionary<string, RequestedAttribute> RequestedAttributes { get; set; } = new Dictionary<string, RequestedAttribute>();
-
-        [JsonProperty("requested_predicates")]
-        public Dictionary<string, RequestedPredicate> RequestedPredicates { get; set; } = new Dictionary<string, RequestedPredicate>();
-    }
+    
 }
