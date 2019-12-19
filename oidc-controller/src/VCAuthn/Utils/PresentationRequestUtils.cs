4908e7a2cdce0d3a2a021c1de513246dace39539
Emiliano Suñé <emiliano.sune@gmail.com>
2019-12-19 09:51:32 -0800

    Code refactoring and cleanup
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- oidc-controller/src/VCAuthn/Utils/PresentationRequestUtils.cs
+++ oidc-controller/src/VCAuthn/Utils/PresentationRequestUtils.cs
@@ -9,36 +9,46 @@ namespace VCAuthn.Utils
     {
         public static string GeneratePresentationRequest(PresentationRequestConfiguration configuration)
         {
-            PresentationRequest_v_1_0 presentationRequest_1_0 = new PresentationRequest_v_1_0();
-            presentationRequest_1_0.Version = configuration.Version;
-            presentationRequest_1_0.Name = configuration.Name;
+            PresentationRequest_v_1_0 presentationRequest_1_0 = new PresentationRequest_v_1_0()
+            {
+                Version = configuration.Version,
+                Name = configuration.Name
+            };
 
-            foreach (RequestedAttribute reqAttribute in configuration.RequestedAttributes) {
+            foreach (RequestedAttribute reqAttribute in configuration.RequestedAttributes)
+            {
                 presentationRequest_1_0.RequestedAttributes.Add(Guid.NewGuid().ToString(), reqAttribute);
             }
-            foreach (RequestedPredicate reqPredicate in configuration.RequestedPredicates) {
+
+            foreach (RequestedPredicate reqPredicate in configuration.RequestedPredicates)
+            {
                 presentationRequest_1_0.RequestedPredicates.Add(Guid.NewGuid().ToString(), reqPredicate);
             }
 
-            Dictionary<string, PresentationRequest_v_1_0> requestBody = new Dictionary<string, PresentationRequest_v_1_0>();
-            requestBody.Add("proof_request", presentationRequest_1_0);
+            Dictionary<string, PresentationRequest_v_1_0> requestBody = new Dictionary<string, PresentationRequest_v_1_0>()
+            {
+                {"proof_request", presentationRequest_1_0}
+            };
             return JsonConvert.SerializeObject(requestBody);
         }
 
-        public static List<PresentationAttachment> GeneratePresentationAttachments(PresentationRequest presentationRequest) {
-            List<PresentationAttachment> attachments = new List<PresentationAttachment>();
-
+        public static List<PresentationAttachment> GeneratePresentationAttachments(PresentationRequest presentationRequest)
+        {
             string base64Payload = presentationRequest.ToJson().ToBase64();
+            PresentationAttachment attachment = new PresentationAttachment()
+            {
+                Id = "libindy-request-presentation-0",
+                MimeType = "application/json",
+                Data = new Dictionary<string, string>()
+                {
+                    {"base64", base64Payload}
+                },
+            };
 
-            PresentationAttachment attachment = new PresentationAttachment();
-            attachment.Id = "libindy-request-presentation-0"; 
-            attachment.MimeType = "application/json";
-            attachment.Data = new Dictionary<string, string>();
-            attachment.Data.Add("base64", base64Payload);
-
-            attachments.Add(attachment);
-            
-            return attachments;
+            return new List<PresentationAttachment>()
+            {
+                attachment
+            };
         }
 
         /// <summary> Utility function that extracts the first Indy <c>Presentationrequest</c> object found
@@ -46,20 +56,27 @@ namespace VCAuthn.Utils
         /// is identified by <c>"@id": "libindy-request-presentation-0"</c>, as specified in the Aries RFC 0037.
         /// <see cref="https://github.com/hyperledger/aries-rfcs/tree/master/features/0037-present-proof#request-presentation"/> 
         /// </summary>
-        public static PresentationRequest ExtractIndyPresentationPequest(List<PresentationAttachment> presentationAttachments) {
+        public static PresentationRequest ExtractIndyPresentationPequest(List<PresentationAttachment> presentationAttachments)
+        {
             PresentationRequest presentationRequest = null;
-            
-            foreach (PresentationAttachment attachment in presentationAttachments) {
-                if (attachment.Id.Equals("libindy-request-presentation-0")) {
+
+            presentationAttachments.ForEach(delegate(PresentationAttachment attachment)
+            {
+                if (attachment.Id.Equals("libindy-request-presentation-0"))
+                {
                     // found first indy presentation attachment, deserialize it so thatit can be returned
-                    string attachmentData = attachment.Data["base64"].FromBase64();
-                    presentationRequest = JsonConvert.DeserializeObject<PresentationRequest>(attachmentData);
+                    presentationRequest = attachment.Data["base64"].toPresentationRequestObject();
                 }
-            }
-            
+            });
+
             return presentationRequest;
+        }
+
+        public static PresentationRequest toPresentationRequestObject(this string attachmentBase64Data)
+        {
+            return JsonConvert.DeserializeObject<PresentationRequest>(attachmentBase64Data.FromBase64());
         }
     }
 
-    
+
 }
