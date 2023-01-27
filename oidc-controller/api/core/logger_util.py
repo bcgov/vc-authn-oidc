e38a1280970a230f87e06b02e132f1da4540ff89
JSyro <jasyrotuck@gmail.com>
2023-01-27 13:23:31 -0800

    add claims to top level if thye match OpenIDSchema
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

---
+++ oidc-controller/api/core/logger_util.py
@@ -0,0 +1,21 @@
+import logging
+import time
+
+
+from typing import Callable, Any
+
+
+def debug_info(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger(func.__name__)
+        logger.setLevel(logging.DEBUG)
+        logger.info(f" >>>> {func.__name__}")
+        logger.debug(f" ..with params={{{args}}}")
+        start_time = time.time()
+        ret_val = func(*args, **kwargs)
+        end_time = time.time()
+        logger.info(f" <<<< {func.__name__} and took {end_time-start_time:.3f} seconds")
+        logger.debug(f" ..with ret_val={{{ret_val}}}")
+        return ret_val
+
+    return wrapper
--- oidc-controller/api/core/oidc/issue_token_service.py
+++ oidc-controller/api/core/oidc/issue_token_service.py
@@ -5,11 +5,14 @@ import dataclasses
 from datetime import datetime
 from typing import List, Dict, Any
 from pydantic import BaseModel
-
+from oic.oic.message import OpenIDSchema
 from ...authSessions.models import AuthSession
 from ...verificationConfigs.models import VerificationConfig
+from ...core.logger_util import debug_info
 
 logger = logging.getLogger(__name__)
+
+PROOF_CLAIMS_ATTRIBUTE_NAME = "vc_presented_attributes"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -26,13 +29,11 @@ class Token(BaseModel):
     claims: Dict[str, Any]
 
     @classmethod
+    @debug_info
     def get_claims(
         cls, pres_exch: Dict, auth_session: AuthSession, ver_config: VerificationConfig
     ) -> List["Claim"]:
         """Converts vc presentation values to oidc claims"""
-        logger.debug(f">>> Token.get_claims")
-        logger.info(pres_exch)
-
         oidc_claims: List[Claim] = [
             Claim(
                 type="pres_req_conf_id",
@@ -46,7 +47,9 @@ class Token(BaseModel):
             Claim(type="nonce", value=auth_session.request_parameters["nonce"])
         )
 
-        presentation_claims: Dict[str, Claim] = {}
+        presentation_claims: Dict[str, Claim] = {
+            "given_name": Claim("email", "email1@net.com")
+        }
         logger.info(
             auth_session.presentation_exchange["presentation_request"][
                 "requested_attributes"
@@ -78,10 +81,9 @@ class Token(BaseModel):
 
         # add sub and append presentation_claims
         oidc_claims.append(Claim(type="sub", value=sub_id_value))
-        presentation_claims.values()
 
         result = {c.type: c.value for c in oidc_claims}
-        result["vc_proof_claims"] = json.dumps(
+        result[PROOF_CLAIMS_ATTRIBUTE_NAME] = json.dumps(
             {c.type: c.value for c in presentation_claims.values()}
         )
         return result
@@ -89,7 +91,7 @@ class Token(BaseModel):
     # renames and calculates dict members appropriate to https://openid.net/specs/openid-connect-core-1_0.html#IDToken
     # and
     # https://github.com/OpenIDC/pyoidc/blob/26ea5121239dad03c5c5551cca149cb984df1ec9/src/oic/oic/message.py#L720
-
+    @debug_info
     def idtoken_dict(self, nonce: str) -> Dict:
         """Converts oidc claims to IdToken attribute names"""
 
@@ -103,6 +105,24 @@ class Token(BaseModel):
         result["nonce"] = nonce
 
         result.update(self.claims)
-        logger.info(f"<<< idtoken_dict r={result}")
+
+        # identify if any standardclaims were provided in the proof and return them at the top level.
+        # https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+        # make copy of dict
+        r2 = result.copy()
+        # add nested values to top level
+        r2.update(json.loads(self.claims[PROOF_CLAIMS_ATTRIBUTE_NAME]))
+        # only keep ones that match the OpenIDschema
+        r2 = {
+            key: r2[key]
+            for key in set(r2.keys()).intersection(set(OpenIDSchema().c_param.keys()))
+        }
+
+        # verify with library schema
+        standard_claims = OpenIDSchema().from_dict(r2)
+        standard_claims.verify()
+        # add to the top level of the dict.
+        for key, value in standard_claims.to_dict().items():
+            result[key] = value
 
         return result
