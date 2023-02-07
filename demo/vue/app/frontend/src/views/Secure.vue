341c360e24423b95a0c590250c1597e7a7658fd0
JSyro <jasyrotuck@gmail.com>
2023-02-06 16:53:15 -0800

    explanation
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- demo/vue/app/frontend/src/views/Secure.vue
+++ demo/vue/app/frontend/src/views/Secure.vue
@@ -19,7 +19,23 @@
       </div>
       <div v-else>Login Valid</div>
 
-      {{ tokenParsed }}
+      <pre>{{ JSON.stringify(tokenParsed, null, 2) }}</pre>
+
+      <ul>
+        <li>If something in the verification configuration is missing, ensure it is being imported to the Keycloak user
+          by the Identity Provider Mapper here. </li>
+        <li><a
+          href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers">http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers</a>
+        </li>
+        <li>and ensure is being added to the token by the client mappers here (click the link, then the 'vue-fe' client,
+          then the 'Mappers' tab). </li>
+        <li><a
+          href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/">http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/4f4d8312-bdd0-4539-8c51-f8de81cf5f41/mapper</a>
+        </li>
+      </li>
+
+
+
     </BaseSecure>
   </v-container>
 </template>
