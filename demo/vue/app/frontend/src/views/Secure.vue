bc77db0bc0ce2e8ea2064ea8a24f7bed99f65cb8
JSyro <jasyrotuck@gmail.com>
2023-02-10 11:43:31 -0800

    instructions in the app.
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- demo/vue/app/frontend/src/views/Secure.vue
+++ demo/vue/app/frontend/src/views/Secure.vue
@@ -2,16 +2,6 @@
   <v-container>
     <BaseSecure>
       <h1 class="my-6 text-center">Secure</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        sollicitudin semper nunc, sed mollis turpis egestas at. Sed ornare,
-        justo non facilisis ullamcorper, eros libero semper ligula, in euismod
-        quam massa et dui. In vel lectus molestie, cursus urna id, vehicula
-        felis. Mauris nec suscipit eros, vel vestibulum ex. Aenean vel gravida
-        nulla. Morbi vel felis at arcu molestie faucibus vitae ut lacus. Nulla
-        feugiat mi quis lectus aliquet, sit amet tristique magna pharetra.
-        Mauris quis felis blandit est hendrerit vulputate.
-      </p>
       <HelloCall />
 
       <div v-if="tokenParsed.pres_req_conf_id != presReqConfId">
@@ -22,20 +12,28 @@
       <pre>{{ JSON.stringify(tokenParsed, null, 2) }}</pre>
 
       <ul>
-        <li>If something in the verification configuration is missing, ensure it is being imported to the Keycloak user
-          by the Identity Provider Mapper here. </li>
-        <li><a
-          href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers">http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers</a>
+        <li>
+          If something in the verification configuration is missing, ensure it
+          is being imported to the Keycloak user by the Identity Provider Mapper
+          here.
         </li>
-        <li>and ensure is being added to the token by the client mappers here (click the link, then the 'vue-fe' client,
-          then the 'Mappers' tab). </li>
-        <li><a
-          href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/">http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/4f4d8312-bdd0-4539-8c51-f8de81cf5f41/mapper</a>
+        <li>
+          <a
+            href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers"
+          >http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-mappers/vc-authn/mappers
+          </a>
         </li>
-      </li>
-
-
-
+        <li>
+          and ensure is being added to the token by the client mappers here
+          (click the link, then the 'vue-fe' client, then the 'Mappers' tab).
+        </li>
+        <li>
+          <a
+            href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/"
+          >http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/clients/4f4d8312-bdd0-4539-8c51-f8de81cf5f41/mapper</a
+          >
+        </li>
+      </ul>
     </BaseSecure>
   </v-container>
 </template>
