a3e39f1af0bab89a78e820d5246a576d23e6b89f
JSyro <jasyrotuck@gmail.com>
2023-02-15 13:06:11 -0800

    don't know what 'claims_interface' is but this loads now
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- oidc-controller/api/core/oidc/provider.py
+++ oidc-controller/api/core/oidc/provider.py
@@ -1,8 +1,9 @@
 import json
+
 from oidcop.server import Server
 
 
 def init_server() -> Server:
-    _str = open(".json").read()
+    _str = open("oidc_op_config.json").read()
     cnf = json.loads(_str)
-    return Server(cnf, cwd="/oidc")
+    return Server(cnf)
--- oidc-controller/api/routers/well_known_oid_config.py
+++ oidc-controller/api/routers/well_known_oid_config.py
@@ -16,20 +16,11 @@ router = APIRouter()
 @router.get("/.well-known/openid-configuration", response_class=JSONResponse)
 async def get_well_known_oid_config(request: Request):
     """returns configuration response compliant with https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationResponse"""
-    # result = ASConfigurationResponse(
-    #     issuer=settings.CONTROLLER_URL,
-    #     authorization_endpoint=settings.CONTROLLER_URL + "/vc/connect/authorize",
-    #     token_endpoint=settings.CONTROLLER_URL + "/vc/connect/token",
-    #     jwks_uri=settings.CONTROLLER_URL + "/.well-known/openid-configuration/jwks",
-    # )
-
     endpoint = request.app.server.server_get("endpoint", "provider_config")
     args = endpoint.process_request()
     response = endpoint.do_response(**args)
     resp = json.loads(response["response"])
     return resp
-
-    # return result
 
 
 @router.get("/.well-known/openid-configuration/jwks", response_class=JSONResponse)
