c95e70f38ac4376bf130aa04a533c9b664a1a294
JSyro <jasyrotuck@gmail.com>
2023-02-17 11:22:10 -0800

    stand up redis as storage for provider.
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- oidc-controller/api/core/oidc/provider.py
+++ oidc-controller/api/core/oidc/provider.py
@@ -1,9 +1,48 @@
-import json
+from jwkest.jwk import rsa_load, RSAKey
 
-from oidcop.server import Server
+from pyop.authz_state import AuthorizationState
+from pyop.provider import Provider
+from pyop.storage import RedisWrapper
+from pyop.subject_identifier import HashBasedSubjectIdentifierFactory
+from pyop.userinfo import Userinfo
+
+from ..config import settings
+
+db_uri = settings.REDISDB_URL
 
 
-def init_server() -> Server:
-    _str = open("oidc_op_config.json").read()
-    cnf = json.loads(_str)
-    return Server(cnf)
+signing_key = RSAKey(key=rsa_load("signing_key.pem"), use="sig", alg="RS256")
+
+configuration_information = {
+    "issuer": "https://controller:5201",
+    "authorization_endpoint": "https://controller:5201/authorization",
+    "token_endpoint": "https://controller:5201/token",
+    "userinfo_endpoint": "https://controller:5201/userinfo",
+    "registration_endpoint": "https://controller:5201/registration",
+    "jwks_uri": "https://controller:5201/.well-known/openid-configuration/jwks",
+    "response_types_supported": ["code", "id_token token"],
+    "id_token_signing_alg_values_supported": [signing_key.alg],
+    "response_modes_supported": ["fragment", "query"],
+    "subject_types_supported": ["public", "pairwise"],
+    "grant_types_supported": ["authorization_code", "implicit"],
+    "claim_types_supported": ["normal"],
+    "claims_parameter_supported": True,
+    "claims_supported": ["sub", "name", "given_name", "family_name"],
+    "request_parameter_supported": False,
+    "request_uri_parameter_supported": False,
+    "scopes_supported": ["openid", "profile"],
+}
+
+subject_id_factory = HashBasedSubjectIdentifierFactory("asdwadwa")
+authz_state = AuthorizationState(
+    subject_id_factory,
+    RedisWrapper(db_uri, db_name="provider", collection="authz_codes"),
+    RedisWrapper(db_uri, db_name="provider", collection="access_tokens"),
+    RedisWrapper(db_uri, db_name="provider", collection="refresh_tokens"),
+    RedisWrapper(db_uri, db_name="provider", collection="subject_identifiers"),
+)
+client_db = RedisWrapper(db_uri, db_name="provider", collection="clients")
+user_db = RedisWrapper(db_uri, db_name="provider", collection="users")
+provider = Provider(
+    signing_key, configuration_information, authz_state, client_db, Userinfo(user_db)
+)
--- oidc-controller/api/routers/well_known_oid_config.py
+++ oidc-controller/api/routers/well_known_oid_config.py
@@ -3,30 +3,30 @@ import logging, json
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 
-
 # from oic.oauth2.message import ASConfigurationResponse
 from jwkest.jwk import RSAKey, KEYS
-
+from ..core.oidc import provider
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
+key = RSAKey().load("signing_key.pem")
+jwks = KEYS()
+jwks.append(key)
+
 
 @router.get("/.well-known/openid-configuration", response_class=JSONResponse)
 async def get_well_known_oid_config(request: Request):
     """returns configuration response compliant with https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationResponse"""
-    endpoint = request.app.server.server_get("endpoint", "provider_config")
-    args = endpoint.process_request()
-    response = endpoint.do_response(**args)
-    resp = json.loads(response["response"])
-    return resp
+    return provider.configuration_information
+
+
+@router.get("/authorize")
+async def authorize(request: Request):
+    return {}
 
 
 @router.get("/.well-known/openid-configuration/jwks", response_class=JSONResponse)
 async def get_well_known_oid_config():
-    """returns configuration response compliant with https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationResponse"""
-    key = RSAKey().load("signing_key.pem")
-    jwks = KEYS()
-    jwks.append(key)
-    return json.loads(jwks.dump_jwks())
+    return provider.signing_key.to_dict()
