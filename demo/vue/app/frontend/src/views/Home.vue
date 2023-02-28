6da978b3ba6b47c5cb9377e00ddd5cb36e85a8cf
JSyro <jasyrotuck@gmail.com>
2023-02-28 13:23:29 -0800

    new kc config
    auth as basic auth now. 
    
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- demo/vue/app/frontend/src/views/Home.vue
+++ demo/vue/app/frontend/src/views/Home.vue
@@ -2,14 +2,14 @@
   <v-container>
     <h1 class="my-6 text-center">Welcome to vc-authn-oidc demo application.</h1>
     <h2>Pre-requisites</h2>
-    <br/>
-    <h4>Mobile Wallet</h4>    
+    <br />
+    <h4>Mobile Wallet</h4>
     <ul>
       <li>
         Download BC Wallet to a mobile device
         <a
           href="https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet"
-        >https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet</a
+          >https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet</a
         >
       </li>
       <li>
@@ -35,6 +35,14 @@
         '/ver_configs' with the default payload
       </li>
       <li>
+        Navigate to the
+        <a
+          href="http://localhost:8880/auth/admin/master/console/#/realms/vc-authn/identity-provider-settings/provider/oidc/vc-authn"
+          >keycloak idp config</a
+        >
+        and set the client secret to "12345678"
+      </li>
+      <li>
         Click "Login" in the top right, then click "Verifiable Credential
         Access"
       </li>
@@ -42,7 +50,10 @@
     <br />
     <br />
 
-    <h5>Any improvements to the demo setup to reduce/streamline these steps are great tasks for future developers</h5>
+    <h5>
+      Any improvements to the demo setup to reduce/streamline these steps are
+      great tasks for future developers
+    </h5>
     <HelloWorld />
   </v-container>
 </template>
