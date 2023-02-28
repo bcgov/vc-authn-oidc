154526eee9ba795e7863f3bd48bf81b5a6e5c6ac
JSyro <jasyrotuck@gmail.com>
2023-02-28 13:57:56 -0800

    remove unused but add link
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- oidc-controller/api/core/oidc/provider.py
+++ oidc-controller/api/core/oidc/provider.py
@@ -28,46 +28,8 @@ if urlparse(issuer_url).scheme != "https":
 signing_key = RSAKey(key=rsa_load("signing_key.pem"), use="sig", alg="RS256")
 signing_keys = KEYS().append(signing_key)
 
-# config from vc-authn-oidc 1.0
-# configuration_information = {
-#     "issuer": issuer_url,
-#     "authorization_endpoint": f"{issuer_url}/authorization",
-#     "token_endpoint": f"{issuer_url}/token",
-#     "jwks_uri": f"{issuer_url}/.well-known/openid-configuration/jwks",
-#     "id_token_signing_alg_values_supported": [signing_key.alg],
-#     "response_modes_supported": ["form_post", "query", "fragment"],
-#     "claims_supported": [],
-#     "scopes_supported": ["openid", "profile", "offline_access"],
-#     "grant_types_supported": [
-#         "authorization_code",
-#         "client_credentials",
-#         "refresh_token",
-#         "implicit",
-#         "urn:ietf:params:oauth:grant-type:device_code",
-#     ],
-#     "response_types_supported": [
-#         "code",
-#         "token",
-#         "id_token",
-#         "id_token token",
-#         "code id_token",
-#         "code token",
-#         "code id_token token",
-#     ],
-#     "token_endpoint_auth_methods_supported": [
-#         "client_secret_basic",
-#         "client_secret_post",
-#         "client_secret_query",
-#     ],
-#     "code_challenge_methods_supported": ["plain", "S256"],
-#     "request_parameter_supported": True,
-#     "frontchannel_logout_supported": True,
-#     "frontchannel_logout_session_supported": True,
-#     "backchannel_logout_supported": True,
-#     "backchannel_logout_session_supported": True,
-# }
+# config from vc-authn-oidc 1.0 https://toip-vc-authn-controller-dev.apps.silver.devops.gov.bc.ca/.well-known/openid-configuration
 
-# # TO BE REVIEWED
 configuration_information = {
     "issuer": issuer_url,
     "authorization_endpoint": f"{issuer_url}/authorization",
@@ -114,4 +76,3 @@ provider = Provider(
     {"keycloak": kc_client},
     Userinfo({"Jason": {"sub": "Jason"}}),
 )
-print(provider.clients)
