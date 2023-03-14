f87937f527335c22e0c49fd51981aa341301571b
JSyro <jasyrotuck@gmail.com>
2023-03-14 12:10:17 -0700

    use controller host and update poll url
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- oidc-controller/api/routers/oidc.py
+++ oidc-controller/api/routers/oidc.py
@@ -84,13 +84,14 @@ async def get_authorize(request: Request, db: Database = Depends(get_db)):
     qrcode.make(url_to_message).save(buff, format="PNG")
     image_contents = base64.b64encode(buff.getvalue()).decode("utf-8")
 
-    callback_url = f"http://localhost:5201{AuthorizeCallbackUri}?pid={auth_session.id}"
-
+    callback_url = (
+        f"http://{controller_host}{AuthorizeCallbackUri}?pid={auth_session.id}"
+    )
     return f"""
     <html>
         <script>
         setInterval(function() {{
-            fetch('{controller_host}/vc/connect{ChallengePollUri}/{auth_session.pres_exch_id}')
+            fetch('{controller_host}{ChallengePollUri}/{auth_session.pres_exch_id}')
                 .then(response => response.json())
                 .then(data => {{if (data.verified) {{
                         window.location.replace('{controller_host}{AuthorizeCallbackUri}
