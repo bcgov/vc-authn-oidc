2272bd1559856979d312d5a23a89f0f614c62bef
JSyro <jasyrotuck@gmail.com>
2023-03-22 10:10:56 -0700

    uuid id's are now 'id' as the local var in CRUD methods.
    Signed-off-by: JSyro <jasyrotuck@gmail.com>

--- oidc-controller/api/authSessions/crud.py
+++ oidc-controller/api/authSessions/crud.py
@@ -13,7 +13,6 @@ from .models import (
     AuthSessionPatch,
 )
 from api.db.session import COLLECTION_NAMES
-from api.core.oidc.provider import init_provider
 
 
 logger = logging.getLogger(__name__)
@@ -28,13 +27,13 @@ class AuthSessionCRUD:
         result = col.insert_one(jsonable_encoder(auth_session))
         return AuthSession(**col.find_one({"_id": result.inserted_id}))
 
-    async def get(self, auth_session_id: str) -> AuthSession:
-        if not PyObjectId.is_valid(auth_session_id):
+    async def get(self, id: str) -> AuthSession:
+        if not PyObjectId.is_valid(id):
             raise HTTPException(
                 status_code=http_status.HTTP_400_BAD_REQUEST, detail=f"Invalid id: {id}"
             )
         col = self._db.get_collection(COLLECTION_NAMES.AUTH_SESSION)
-        auth_sess = col.find_one({"_id": PyObjectId(auth_session_id)})
+        auth_sess = col.find_one({"_id": PyObjectId(id)})
 
         if auth_sess is None:
             raise HTTPException(
@@ -44,23 +43,23 @@ class AuthSessionCRUD:
 
         return AuthSession(**auth_sess)
 
-    async def patch(self, auth_session_id: str, data: AuthSessionPatch) -> AuthSession:
+    async def patch(self, id: str, data: AuthSessionPatch) -> AuthSession:
         col = self._db.get_collection(COLLECTION_NAMES.AUTH_SESSION)
         auth_sess = col.find_one_and_update(
-            {"_id": auth_session_id},
+            {"_id": id},
             {"$set": data.dict(exclude_unset=True)},
             return_document=ReturnDocument.AFTER,
         )
 
         return auth_sess
 
-    async def delete(self, auth_session_id: str) -> bool:
+    async def delete(self, id: str) -> bool:
         if not PyObjectId.is_valid(id):
             raise HTTPException(
                 status_code=http_status.HTTP_400_BAD_REQUEST, detail=f"Invalid id: {id}"
             )
         col = self._db.get_collection(COLLECTION_NAMES.AUTH_SESSION)
-        auth_sess = col.find_one_and_delete({"_id": PyObjectId(auth_session_id)})
+        auth_sess = col.find_one_and_delete({"_id": PyObjectId(id)})
         return bool(auth_sess)
 
     async def get_by_pres_exch_id(self, pres_exch_id: str) -> AuthSession:
