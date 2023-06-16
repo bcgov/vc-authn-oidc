65ae0e992462702621a478aec139d015988c4249
Jamie Popkin <popkinj@littleearth.ca>
2023-06-16 15:19:18 -0700

    Think this is good for storing timeout values
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- oidc-controller/api/routers/acapy_handler.py
+++ oidc-controller/api/routers/acapy_handler.py
@@ -10,6 +10,8 @@ from ..authSessions.models import (AuthSession, AuthSessionPatch,
 from ..core.acapy.client import AcapyClient
 from ..db.session import get_db
 
+from ..core.config import settings
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
@@ -27,12 +29,16 @@ async def post_topic(request: Request, topic: str, db: Database = Depends(get_db
     match topic:
         case "present_proof":
             webhook_body = await _parse_webhook_body(request)
+            print('webhook_body', webhook_body)
+            print('created_at', webhook_body.get('created_at'))
+            print('timeout value', settings.CONTROLLER_PRESENTATION_EXPIRE_TIME)
             logger.info(
                 f">>>> pres_exch_id: {webhook_body['presentation_exchange_id']}"
             )
             auth_session: AuthSession = await AuthSessionCRUD(db).get_by_pres_exch_id(
                 webhook_body["presentation_exchange_id"]
             )
+
             if webhook_body["state"] == "presentation_received":
                 logger.info("GOT A PRESENTATION, TIME TO VERIFY")
                 client.verify_presentation(auth_session.pres_exch_id)
