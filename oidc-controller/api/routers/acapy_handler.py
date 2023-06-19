ed9dab6ac211d61a636502fff025d85f4fc5c401
Jamie Popkin <popkinj@littleearth.ca>
2023-06-19 14:32:48 -0700

    Calculating the timeout
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- oidc-controller/api/routers/acapy_handler.py
+++ oidc-controller/api/routers/acapy_handler.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from fastapi import APIRouter, Depends, Request
 from pymongo.database import Database
@@ -30,12 +30,28 @@ async def post_topic(request: Request, topic: str, db: Database = Depends(get_db
     match topic:
         case "present_proof":
             webhook_body = await _parse_webhook_body(request)
-            print('webhook_body', webhook_body)
-            print('created_at', webhook_body.get('created_at'))
-            print('timeout value', settings.CONTROLLER_PRESENTATION_EXPIRE_TIME)
             logger.info(
                 f">>>> pres_exch_id: {webhook_body['presentation_exchange_id']}"
             )
+            created_time = datetime.fromisoformat(webhook_body.get('created_at'))
+
+            expired_time  = created_time + timedelta(seconds=settings.CONTROLLER_PRESENTATION_EXPIRE_TIME)
+            now_time = datetime.now(timezone.utc)
+            print('time difference', expired_time < now_time)
+            # TODO: This logic should also be added to the polling logic
+            # TODO: And possibly add the timeout value to the AuthSession model
+            # print('time difference', expired_time > datetime.now())
+            # calculate the number of seconds between two times
+            # time_delta = datetime.now() - created_time
+            # print('time_diff', time_delta.total_seconds())
+
+            # print('expired_time', expired_time)
+
+            print('timeout value', settings.CONTROLLER_PRESENTATION_EXPIRE_TIME)
+            print('webhook_body', webhook_body)
+            print('created_at', webhook_body.get('created_at'))
+            print('now', datetime.now(timezone.utc))
+
             auth_session: AuthSession = await AuthSessionCRUD(db).get_by_pres_exch_id(
                 webhook_body["presentation_exchange_id"]
             )
