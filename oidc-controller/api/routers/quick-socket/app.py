565b7aa33fc607b6346c8bf9bd4738913193670a
Jamie Popkin <popkinj@littleearth.ca>
2023-06-29 15:28:42 -0700

    Touchdown 🙌 🏈 🎯 websockets are working
    In the test script at least
    
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- oidc-controller/api/routers/quick-socket/app.py
+++ oidc-controller/api/routers/quick-socket/app.py
@@ -1,28 +1,67 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import socketio # Installed with 'pip install python-socketio`
+
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Socket.io Test</title>
+    <script src="https://cdn.socket.io/4.3.2/socket.io.min.js"></script>
+    <script>
+        const socket = io("ws://localhost:5100", {
+        path: "/ws/socket.io",
+        autoConnect: false,
+        });
+
+        const handleConnect = () => {
+            console.log("Button Clicked to Connect");
+            socket.connect();
+        };
+
+        const handleDisconnect = () => {
+            console.log("Button Clicked to Disconnect");
+            socket.disconnect();
+        };
+
+        socket.on("message", (e) => console.log("Received", e));
+    </script>
+</head>
+<body>
+    <div className="App">
+        <button onClick="handleConnect()">Connect</button>
+        <button onClick="handleDisconnect()">Disconnect</button>
+    </div>
+</body>
+</html>
+"""
 
 # Create a FastAPI instance
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
 # Create a test websocket server
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
-app.mount('/wd', sio)
 
 @sio.event
-def connect(sid, socket):
+async def connect(sid, socket):
     print('connected', sid)
+    await sio.emit('message', {'data': "I'm a real boy!'"})
 
 @sio.event
 def disconnected(sid):
     print('disconnected', sid)
 
-@sio.event
-def get_name(sid, data):
-    print('get_name', data)
-    sio.emit('name', data)
+sio_app = socketio.ASGIApp(socketio_server=sio)
 
-uvicorn.run(app, host="0.0.0.0", port=5100)
+app.mount('/ws', sio_app)
+
+uvicorn.run(app, host="localhost", port=5100)
 
 # TODO1: Test with websocat
 # TODO2: Integrate into acapy_handler.py
