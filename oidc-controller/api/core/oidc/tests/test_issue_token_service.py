22e28f4bf95e763476cd6ceff0893a38f2e41142
jamshale <jamiehalebc@gmail.com>
2023-09-22 14:01:19 -0700

    Add backwards compatibility setting for token claims
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- oidc-controller/api/core/oidc/tests/test_issue_token_service.py
+++ oidc-controller/api/core/oidc/tests/test_issue_token_service.py
@@ -4,6 +4,7 @@ from api.authSessions.models import AuthSession
 from api.core.oidc.issue_token_service import Token
 from api.core.oidc.tests.__mocks__ import auth_session, presentation, ver_config
 from api.test_utils import is_valid_uuid
+from api.core.config import settings
 
 basic_valid_requested_attributes = {
     "req_attr_0": {
@@ -24,6 +25,34 @@ basic_valid_revealed_attr_groups = {
             "email": {
                 "raw": "jamiehalebc@gmail.com",
                 "encoded": "73814602767252868561268261832462872577293109184327908660400248444458427915643",
+            }
+        }
+    }
+}
+
+multiple_valid_requested_attributes = {
+    "req_attr_0": {
+        "names": ["email_1", "age_1"],
+        "restrictions": [
+            {
+                "schema_name": "verified-email",
+                "issuer_did": "MTYqmTBoLT7KLP5RNfgK3b",
+            }
+        ],
+    },
+}
+
+multiple_valid_revealed_attr_groups = {
+    "req_attr_0": {
+        "sub_proof_index": 0,
+        "values": {
+            "email_1": {
+                "raw": "jamiehalebc@gmail.com",
+                "encoded": "73814602767252868561268261832462872577293109184327908660400248444458427915643",
+            },
+            "age_1": {
+                "raw": "30",
+                "encoded": "73814602767252868561268261832462872577293109184327908660400248444458427915644",
             }
         }
     }
@@ -87,36 +116,47 @@ async def test_valid_proof_presentation_with_multiple_attributes_returns_claims(
 
 
 @pytest.mark.asyncio
-async def test_valid_proof_presentation_with_one_attribute_and_multiple_values_returns_claims():
-    presentation['presentation_request']['requested_attributes'] = {
-        "req_attr_0": {
-            "names": ["email_1", "age_1"],
-            "restrictions": [
-                {
-                    "schema_name": "verified-email",
-                    "issuer_did": "MTYqmTBoLT7KLP5RNfgK3b",
-                }
-            ],
-        },
-    }
-    presentation['presentation']['requested_proof']['revealed_attr_groups'] = {
-        "req_attr_0": {
-            "sub_proof_index": 0,
-            "values": {
-                "email_1": {
-                    "raw": "jamiehalebc@gmail.com",
-                    "encoded": "73814602767252868561268261832462872577293109184327908660400248444458427915643",
-                },
-                "age_1": {
-                    "raw": "30",
-                    "encoded": "73814602767252868561268261832462872577293109184327908660400248444458427915644",
-                }
-            }
-        }
-    }
+@mock.patch.object(settings, "USE_V1_COMPATIBILITY", False)
+async def test_use_v1_compatibility_false_does_not_add_the_named_attributes():
+    presentation['presentation_request']['requested_attributes'] = multiple_valid_requested_attributes
+    presentation['presentation']['requested_proof']['revealed_attr_groups'] = multiple_valid_revealed_attr_groups
     with mock.patch.object(AuthSession, "presentation_exchange", presentation):
         claims = Token.get_claims(auth_session, ver_config)
+        vc_presented_attributes_obj = eval(claims["vc_presented_attributes"])
         assert claims is not None
+        assert vc_presented_attributes_obj["email_1"] == 'jamiehalebc@gmail.com'
+        assert vc_presented_attributes_obj["age_1"] == '30'
+        assert hasattr(claims, "email_1") is False
+        assert hasattr(claims, "age_1") is False
+
+
+@pytest.mark.asyncio
+@mock.patch.object(settings, "USE_V1_COMPATIBILITY", True)
+async def test_use_v1_compatibility_true_adds_the_named_attributes():
+    presentation['presentation_request']['requested_attributes'] = multiple_valid_requested_attributes
+    presentation['presentation']['requested_proof']['revealed_attr_groups'] = multiple_valid_revealed_attr_groups
+    with mock.patch.object(AuthSession, "presentation_exchange", presentation):
+        claims = Token.get_claims(auth_session, ver_config)
+        vc_presented_attributes_obj = eval(claims["vc_presented_attributes"])
+        assert claims is not None
+        assert vc_presented_attributes_obj["email_1"] == 'jamiehalebc@gmail.com'
+        assert vc_presented_attributes_obj["age_1"] == '30'
+        assert claims["email_1"] == 'jamiehalebc@gmail.com'
+        assert claims["age_1"] == '30'
+
+@pytest.mark.asyncio
+@mock.patch.object(settings, "USE_V1_COMPATIBILITY", None)
+async def test_use_v1_compatibility_none_does_not_add_the_named_attributes():
+    presentation['presentation_request']['requested_attributes'] = multiple_valid_requested_attributes
+    presentation['presentation']['requested_proof']['revealed_attr_groups'] = multiple_valid_revealed_attr_groups
+    with mock.patch.object(AuthSession, "presentation_exchange", presentation):
+        claims = Token.get_claims(auth_session, ver_config)
+        vc_presented_attributes_obj = eval(claims["vc_presented_attributes"])
+        assert claims is not None
+        assert vc_presented_attributes_obj["email_1"] == 'jamiehalebc@gmail.com'
+        assert vc_presented_attributes_obj["age_1"] == '30'
+        assert hasattr(claims, "email_1") is False
+        assert hasattr(claims, "age_1") is False
 
 
 @pytest.mark.asyncio
@@ -156,6 +196,7 @@ async def test_valid_presentation_with_matching_subject_identifier_has_identifie
         claims = Token.get_claims(auth_session, ver_config)
         print(claims)
         assert claims["sub"] == "jamiehalebc@gmail.com"
+
 
 @pytest.mark.asyncio
 async def test_valid_presentation_with_non_matching_subject_identifier_and_has_uuid_in_claims_sub():
