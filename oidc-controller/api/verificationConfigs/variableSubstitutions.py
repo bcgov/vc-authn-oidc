4d5b0ba4c1bf6b2cf18734ee583462c51e16837a
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2024-10-30 11:57:29 -0700

    relocate exec to prevent accessing a closed file
    Signed-off-by: Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>

--- oidc-controller/api/verificationConfigs/variableSubstitutions.py
+++ oidc-controller/api/verificationConfigs/variableSubstitutions.py
@@ -115,7 +115,7 @@ variable_substitution_map = VariableSubstitutionMap()
 def apply_user_variables():
     try:
         with open(settings.CONTROLLER_VARIABLE_SUBSTITUTION_OVERRIDE) as user_file:
-            og_substitution_map = copy.copy(variable_substitution_map.user_static_map)
+            code = user_file.read()
     except TypeError:
         logger.warning("No user defined variable substitutions file provided")
         return None
@@ -123,7 +123,8 @@ def apply_user_variables():
         logger.warning("User defined variable substitutions file could not be found")
         return None
     else:
-        exec(user_file.read())
+        og_substitution_map = copy.copy(variable_substitution_map.user_static_map)
+        exec(code)
         if len(variable_substitution_map.user_static_map) <= 1:
             logger.info("No new user created variable substitution where found")
         for pattern, func in variable_substitution_map.user_static_map.items():
